5f1e8ecf74264e4e17a77dd0912be72d29a28aa4
shbunch <shawnbunch@gmail.com>
2023-09-17 23:59:20 -0500

    Added usetate

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom';
 import LogoTitle from '../../assets/images/logo-s.png';
 import './index.scss';
+import { useState } from 'react';
 
 const Home = () => {
-  
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['l', 'o', 'b', 'o', 'd', 'a', 'n']
 
     return (
       <div className="container home-page">
