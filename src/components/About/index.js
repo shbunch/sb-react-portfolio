edaba6a5a888c84b6f333aa41479fbcd1024a1ed
shbunch <shawnbunch@gmail.com>
2023-09-23 18:58:33 -0500

    Updated rotating cube and styling

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -50,19 +50,19 @@ const About = () => {
             <FontAwesomeIcon icon={faAngular} color="#DD0031" />
           </div>
           <div className='face2'>
-            <FontAwesomeIcon icon={faHtml5} color="#DD0031" />
+            <FontAwesomeIcon icon={faHtml5} color="#F06529" />
           </div>
           <div className='face3'>
-            <FontAwesomeIcon icon={faCss3} color="#DD0031" />
+            <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
           </div>
           <div className='face4'>
-            <FontAwesomeIcon icon={faReact} color="#DD0031" />
+            <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
           </div>
           <div className='face5'>
-            <FontAwesomeIcon icon={faJsSquare} color="#DD0031" />
+            <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
           </div>
           <div className='face6'>
-            <FontAwesomeIcon icon={faGitAlt} color="#DD0031" />
+            <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
           </div>
         </div>
       </div>
