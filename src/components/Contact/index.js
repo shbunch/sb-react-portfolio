01e5e0e8bc5e9a00986918a176b0a3edef787e03
shbunch <shawnbunch@gmail.com>
2023-09-24 23:49:18 -0500

    Added Contact page

---
+++ src/components/Contact/index.js
@@ -0,0 +1,23 @@
+import Loader from 'react-loaders'
+import './index.scss'
+import AnimatedLetters from '../AnimatedLetters';
+
+const Contact = () => {
+
+  return (
+    <>
+      <div className='container contact-page'>
+        <div className='text-zone'>
+          <h1>
+            <AnimatedLetters strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'M', 'e']} 
+            idx={15}
+            />
+          </h1>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default Contact
