25c82e819b566bcf87d61e1b9e968d52a7f8d63c
shbunch <shawnbunch@gmail.com>
2023-09-25 00:14:53 -0500

    Added contact form fields

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -18,11 +18,44 @@ const Contact = () => {
         <div className='text-zone'>
           <h1>
             <AnimatedLetters
-            letterClass={letterClass}
-            strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'M', 'e']} 
-            idx={15}
+              letterClass={letterClass}
+              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'M', 'e']}
+              idx={15}
             />
           </h1>
+          <p>
+            If you have other requests or questions, please contact me using the form below
+          </p>
+          <div className='contact-form'>
+            <form>
+              <ul>
+                <li className='half'>
+                  <input type="text" name="name" placeholder="Name" required />
+                </li>
+                <li className='half'>
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    required />
+                </li>
+                <li>
+                  <input
+                    placeholder="Subject"
+                    type="text"
+                    name="subject"
+                    required />
+                </li>
+                <li>
+                  <textarea
+                    placeholder="Message"
+                    name="message"
+                    required
+                  ></textarea>
+                </li>
+              </ul>
+            </form>
+          </div>
         </div>
       </div>
       <Loader type="pacman" />
