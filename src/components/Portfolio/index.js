bae5b097ff8ad983519fbaa03d543e47fd366d9c
shbunch <shawnbunch@gmail.com>
2023-09-27 14:34:51 -0500

    Adding other projects and images and adding them to portfolio page

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -27,9 +27,17 @@ const Portfolio = () => {
             return (
               <div className="image-box" key={idx}>
                 <img
-                src={port.cover}
-                className="portfolio-image"
-                alt="portfolio" />
+                  src={port.cover}
+                  className="portfolio-image"
+                  alt="portfolio" />
+                <div className="content">
+                  <p className="title">{port.title}</p>
+                  <h4 className="description">{port.description}</h4>
+                  <button
+                    className="btn"
+                    onClick={() => window.open(port.url)}
+                  >View</button>
+                </div>
               </div>
             )
           })
