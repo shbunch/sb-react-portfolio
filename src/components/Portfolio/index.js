9ae7ebd482397b2bdaaf36bfa5377febf255e9f8
shbunch <shawnbunch@gmail.com>
2023-09-27 11:38:26 -0500

    Adding portfolio data and rendering image to page

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,20 +1,54 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './index.scss';
 import Loader from "react-loaders";
 import AnimatedLetters from '../AnimatedLetters'
+import portfolioData from '../../data/portfolio.json'
 
 const Portfolio = () => {
   const [letterClass, setletterClass] = useState('text-animate')
+  console.log(portfolioData);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setletterClass('text-animate-hover');
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    }
+  })
+
+
+  const renderPortfolio = (portfolio) => {
+    return (
+      <div className="images-container">
+        {
+          portfolio.map((port, idx) => {
+            return (
+              <div className="image-box" key={idx}>
+                <img
+                src={port.cover}
+                className="portfolio-image"
+                alt="portfolio" />
+              </div>
+            )
+          })
+        }
+      </div>
+    )
+  }
+
   return (
     <>
       <div className="container portfolio-page">
         <h1 className="page-title">
           <AnimatedLetters
-            letterClass={letterClass} 
+            letterClass={letterClass}
             strArray={"Portfolio".split("")}
             idx={15}
-          />  
+          />
         </h1>
+        <div>{renderPortfolio(portfolioData.portfolio)}</div>
       </div>
       <Loader type="pacman" />
     </>
