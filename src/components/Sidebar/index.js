74335f84d64829809ae6eb96013950b844bd7681
shbunch <shawnbunch@gmail.com>
2023-09-27 16:10:00 -0500

    Removed import for developer logo

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,7 @@
 import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
 import LogoS from '../../assets/images/logo-s.png'
-import LogoSubtitle from '../../assets/images/logo_sub.png'
+// import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faSuitcase, faUser } from '@fortawesome/free-solid-svg-icons' 
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
