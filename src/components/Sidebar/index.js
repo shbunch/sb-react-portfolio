4bfa3277373f8e3fcd6818cf19c2481c27db8fd9
shbunch <shawnbunch@gmail.com>
2023-10-12 18:54:02 -0500

    Added close button for sidebar and beginning to style the homepage on mobile

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,7 +3,7 @@ import './index.scss'
 import LogoS from '../../assets/images/logo-s.png'
 // import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars, faEnvelope, faHome, faSuitcase, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faClose, faEnvelope, faHome, faSuitcase, faUser } from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { useState } from 'react'
 
@@ -29,6 +29,13 @@ const Sidebar = () => {
         <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
           <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
         </NavLink>
+        <FontAwesomeIcon
+          onClick={() => setShowNav(false)}
+          icon={faClose}
+          color="#ffd700"
+          size="3x"
+          className='close-icon'
+        />
       </nav>
       <ul>
         <li>
